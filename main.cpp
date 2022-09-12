32b5dcf5085462904df4b51b6c10f539cb78ac27
Jaime-prog <A00571842@tec.mx>
2022-09-12 15:07:51 -0500

    corrected output file

--- main.cpp
+++ main.cpp
@@ -126,7 +126,6 @@ string longestSubstring(string text1, string text2)
     return longestSubstring;
 }
 
-
 // function to print vector
 template <typename T>
 void printVector(vector<T> vec)
@@ -137,6 +136,21 @@ void printVector(vector<T> vec)
     }
     cout << endl;
 }
+
+// // write file
+// void writeFile(string fileName, string text)
+// {
+//     ofstream out(fileName);
+//     if (out.is_open())
+//     {
+//         out << text;
+//         out.close();
+//     }
+//     else
+//     {
+//         cout << "Unable to open file";
+//     }
+// }
 
 int main()
 {
@@ -160,39 +174,73 @@ int main()
     int resultText2Code2 = isSubstring(text2, code2);
     int resultText2Code3 = isSubstring(text2, code3);
 
+    ofstream solutionFile("mysolution.txt");
+
     // ======1st part of the code======
     // transmission 1.txt
     cout << "transmission1.txt: " << endl;
+    solutionFile << "transmission1.txt: " << endl;
     cout << "mcode1.txt - ";
+    solutionFile << "mcode1.txt - ";
     resultText1Code1 == -1 ? cout << "false" : cout << "true, start at position " << resultText1Code1 << endl;
+    resultText1Code1 == -1 ? solutionFile << "false" : solutionFile << "true, start at position " + to_string(resultText1Code1);
     cout << "mcode2.txt - ";
+    solutionFile << endl
+                 << "mcode2.txt - ";
     resultText1Code2 == -1 ? cout << "false" : cout << "true, start at position " << resultText1Code2 << endl;
+    resultText1Code1 == -1 ? solutionFile << "false" : solutionFile << "true, start at position " + to_string(resultText1Code2);
     cout << "mcode3.txt - ";
+    solutionFile << endl
+                 << "mcode3.txt - ";
     resultText1Code3 == -1 ? cout << "false" : cout << "true, start at position " << resultText1Code3 << endl;
+    resultText1Code3 == -1 ? solutionFile << "false" : solutionFile << "true, start at position " + to_string(resultText1Code3);
     cout << endl;
+    solutionFile << endl;
+
     // transmission2.txt
     cout << "transmission2.txt: " << endl;
+    solutionFile << endl
+                 << "transmission2.txt: " << endl;
     cout << "mcode1.txt - ";
+    solutionFile << "mcode1.txt - ";
+
     resultText2Code1 == -1 ? cout << "false" : cout << "true, start at position " << resultText2Code1 << endl;
+    resultText2Code1 == -1 ? solutionFile << "false" : solutionFile << "true, start at position " + to_string(resultText2Code1);
     cout << "mcode2.txt - ";
+    solutionFile << endl
+                 << "mcode2.txt - ";
     resultText2Code2 == -1 ? cout << "false" : cout << "true, start at position " << resultText2Code2 << endl;
+    resultText2Code2 == -1 ? solutionFile << "false" : solutionFile << "true, start at position " + to_string(resultText2Code2);
     cout << "mcode3.txt - ";
+    solutionFile << endl
+                 << "mcode3.txt - ";
     resultText2Code3 == -1 ? cout << "false" : cout << "true, start at position " << resultText2Code3 << endl;
+    resultText2Code3 == -1 ? solutionFile << "false" : solutionFile << "true, start at position " + to_string(resultText2Code3);
     cout << endl;
+    solutionFile << endl;
+    solutionFile << endl;
 
     // ======2nd part of the code======
 
     cout << "transmission1.txt: " << endl;
+    solutionFile << "transmission1.txt: " << endl;
     vector<int> palindrome3 = findLongestPalindrome(text);
     cout << "mirrored code found, start at " << palindrome3[0] << " ended at " << palindrome3[1] << endl;
+    solutionFile << "mirrored code found, start at " + to_string(palindrome3[0]) + " ended at " + to_string(palindrome3[1]) << endl;
     vector<int> palindrome4 = findLongestPalindrome(text2);
     cout << endl;
     cout << "transmission2.txt: " << endl;
+    solutionFile << endl
+                 << "transmission2.txt: " << endl;
     cout << "mirrored code found, start at " << palindrome4[0] << " ended at " << palindrome4[1] << endl;
+    solutionFile << "mirrored code found, start at " + to_string(palindrome4[0]) + " ended at " + to_string(palindrome4[1]) << endl;
     cout << endl;
+    solutionFile << endl;
     //======3rd part of the code======
     string longestSubstring1 = longestSubstring(text, text2);
     cout << "the longest common substring between transmission1.txt and transmission2.txt is " << longestSubstring1.length() << " characters long" << endl;
+    solutionFile << "the longest common substring between transmission1.txt and transmission2.txt is " + to_string(longestSubstring1.length()) + " characters long" << endl;
 
+    solutionFile.close();
     return 0;
 }
